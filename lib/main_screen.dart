de09d349c4f2ea98ad6fba4e58f74a2ce1c088d5
Anil Antony <anilvithayathil007@gmail.com>
2024-04-26 13:57:20 +0530

    initial commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_app_clone/core/constants/colors.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: backGroundColor,
+    );
+  }
+}
